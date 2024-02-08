6391db35c7b06842d7ae423bc3bed6cf76713744
Christina Vekri <christina.vek@gmail.com>
2024-02-08 17:10:19 +0100

    cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import pokemonRouter from './routes/pokemon.js';
+import cors from 'cors';
 
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/', pokemonRouter);
 
